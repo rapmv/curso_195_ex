fcb534c564dafc55503048f6572573558135c1a7
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-09-08 16:07:14 -0300

    Exercicio com Interface

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter contract data.");
+		System.out.print("Number: ");
+		Integer number = sc.nextInt();
+		
+		System.out.print("Date(dd/MM/yyy): ");
+		Date date = sdf.parse(sc.next());
+		
+		System.out.print("Contract Value: ");
+		Double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		ContractService cs = new ContractService(new PaypalService());
+		
+		System.out.print("Enter number of installments: ");
+		int N = sc.nextInt();
+		
+		cs.processContract(contract, N);
+		
+		System.out.println("Installments: ");
+		for(Installment it: contract.getInstallments()) {
+			System.out.println(it);
+		}
+		
+		sc.close();
+	}
+
+}
